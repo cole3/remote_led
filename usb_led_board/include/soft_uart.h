dea0ecca74251db7411321796f9c59f6641c0f0d
cole3 <wangchenggang3@gmail.com>
2015-12-06 13:57:42 +0800

    set baud rate 115200, add send input keyevent to android.

--- usb_led_board/include/soft_uart.h
+++ usb_led_board/include/soft_uart.h
@@ -8,6 +8,8 @@ void uart_init(void);
 void uart_deinit(void);
 bit uart_rev(unsigned char *dat);
 bit uart_send(unsigned char dat);
-void print(unsigned char code *str);
+void print_str(unsigned char code *str);
+void print_hex(unsigned char hex);
+void print_dec(unsigned char dec);
 
 #endif
